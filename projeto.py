1437a1e1e14aa43dd8ab35f8a9cd1f8512b56f6c
leandro <leandromrssantos>
2025-05-20 12:38:13 -0300

    documentação adicional

--- projeto.py
+++ projeto.py
@@ -11,10 +11,11 @@ class SauceDemoScraper(BaseCase):
     CSV_FILE = 'products.csv'
 
     def login(self):
-        #realiza login no site
+        #abre a URL do site
         self.open(self.URL)
+        #realiza o login
         self.wait_for_element('#user-name')
         self.send_keys('#user-name', self.USERNAME)
         self.click('#login-button')
-        #verifica login bem-sucedido e aguarda produtos aparecerem
+        #aguarda a lista de produtos para garantir que o login foi bem sucedido
         self.wait_for_element('.inventory_list')

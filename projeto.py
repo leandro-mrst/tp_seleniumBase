896e6a0d85e902d9a0fec45282fd8ce160b88c2f
ps-luana <luanapsantos2005@gmail.com>
2025-05-23 08:23:06 -0300

    projeto.py

--- projeto.py
+++ projeto.py
@@ -37,4 +37,27 @@ class SauceDemoScraper(BaseCase):
                 'preço': price,
             })
         return extracted
+        
+    #função pra salvar dados em um arquivo .csv    
+    def save_to_csv(self, products):
+        if not products:
+            print("Nenhum dado para salvar.")
+            return
+        
+        with open(self.CSV_FILE, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['name', 'description', 'price'])
+            writer.writeheader()
+            #loop para escrever linha no CSV com valores daquele produto, seguindo as colunas
+            for prod in products:
+                writer.writerow(prod)
+        print(f"Dados salvos em {self.CSV_FILE}")
+        
+    #função para adicionar todos os produtos ao carrinho
+    def add_all_to_cart(self):
+        buttons = self.find_elements('.inventory_item button')
+        #loop para clicar em cada botão encontrado na lista .inventory_item button
+        for bot in buttons:
+            bot.click()
+        self.click('.shopping_cart_link')
+        self.wait_for_element('.cart_list')
 
